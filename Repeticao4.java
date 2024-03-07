2022426b0673518696906a60e0abd4b3c2301c1a
Júlia Martins Souza <juliamss07@gmail.com>
2024-03-07 16:59:36 -0300

    Create Repeticao4.java

---
+++ Repeticao4.java
@@ -0,0 +1,19 @@
+//Parte 3-d:
+//Classe ImprimirFatorial
+class ImprimirFatorial {
+    //Bloco main
+	public static void main (String[] args) {
+	    System.out.println("Calcular fatorial de 12");
+	    
+	    //armazena o valor de fatorial
+	    int fatorial = 0;
+	    
+        //calcula o fatorial
+        for (int numero = 0; numero <= 12; numero++) {
+            fatorial += numero;
+        }
+        
+        //imprime o fatorial
+        System.out.println(fatorial);
+	}
+}
