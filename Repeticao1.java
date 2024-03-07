05c9485963843ecf907081bfad7fceabfe9d45fb
Júlia Martins Souza <juliamss07@gmail.com>
2024-03-07 16:43:45 -0300

    Create Repeticao1.java

---
+++ Repeticao1.java
@@ -0,0 +1,16 @@
+//Parte 3-a:
+//importar a classe Scanner
+import java.util.Scanner;
+
+//Classe ImprimirNumeros - Scanner
+class ImprimirNumeros {
+    //Bloco main
+	public static void main (String[] args) {
+	    System.out.println("Imprimir numeros de 100 a 1000");
+	    
+        //imprimir os numeros
+        for (int numero = 100; numero <= 1000; numero++) {
+            System.out.println(numero);
+        }
+	}
+}
