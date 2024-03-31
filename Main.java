7945d2fb4c355183fac6d78cfebc289a45b8cbb1
Júlia Martins Souza <juliamss07@gmail.com>
2024-03-31 18:59:06 -0300

    optimizing main

--- Main.java
+++ Main.java
@@ -1,29 +1,68 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
         Conta minhaConta = new Conta();
 
-        minhaConta.dono = "João";
+        minhaConta.dono = "Julia";
         minhaConta.agencia = "222-3";
         minhaConta.numero = 1234;
-        minhaConta.saldo = 200;
+        minhaConta.saldo = 2000;
+        minhaConta.limite = 2500;
+        
+        int executar;
+        
+        Scanner input = new Scanner(System.in);
 
-        boolean depositoRealizado = minhaConta.depositar(250.0);
-
-        if (depositoRealizado) {
-            System.out.println("Deposito realizado com sucesso!");
-            System.out.println("Saldo atual: " +minhaConta.saldo);
-        } else {
-            System.out.println("Erro ao depositar...");
-        }
-
-        boolean saqueRealizadp = minhaConta.sacar(300.0);
-
-        if (saqueRealizadp) {
-            System.out.println("Saque realizado com sucesso!");
-            System.out.println("Saldo atual: " +minhaConta.saldo);
-        } else {
-            System.out.println("Erro ao sacar...");
-        }
+        do {
+            System.out.println("\n\nSITE OFICIAL BANCO JMS\n O que deseja fazer?");
+            System.out.println("[1] Deposito\n[2] Saque\n[3] Imprimir Infos\n[0] Sair");
+            
+            executar = input.nextInt();
+            
+            switch (executar) {
+                case 1:
+                    int valorDep;
+                    
+                    System.out.println("\nQual o valor do deposito?");
+                    valorDep = input.nextInt();
+                    
+                    boolean depositoRealizado = minhaConta.depositar(valorDep);
+            
+                    if (depositoRealizado) {
+                        System.out.println("Deposito realizado com sucesso!");
+                        System.out.println("Saldo atual: " +minhaConta.saldo);
+                    } else {
+                        System.out.println("Erro ao depositar...");
+                    }
+                break;
+                
+                case 2:
+                    int valorSaq;
+                    
+                    System.out.println("\nQual o valor do saque?");
+                    valorSaq = input.nextInt();
+                    
+                    boolean saqueRealizado = minhaConta.sacar(valorSaq);
+            
+                    if (saqueRealizado) {
+                        System.out.println("Saque realizado com sucesso!");
+                        System.out.println("Saldo atual: " +minhaConta.saldo);
+                    } else {
+                        System.out.println("Erro ao sacar...");
+                    }
+                break;
+                
+                case 3:
+                
+                    System.out.println("\nImprimindo informacoes:");
+                    boolean imprimir = minhaConta.imprimir();
+                    break;
+                
+                case 0:
+                break;
+            }
+        } while (executar!=0);
     }
 }
